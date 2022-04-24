06e432f1e77841288139bc55836c1eccab5da9f6
Leonardo Nakahara <leonardo.nakahara@gmail.com>
2022-04-24 17:44:14 -0300

    Atuaização de Classe para App em App.js

--- monster-rolodex-app/src/App.js
+++ monster-rolodex-app/src/App.js
@@ -1,76 +1,46 @@
-import { Component } from 'react';
-
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 import './App.css';
 
 const App = () => {
+  // Essa constante é equivalente à inicialização do state quando usando classes.
+  // A diferença é que isso fará com que tenhamos mais código.
+  const [nomeBuscado, setNomeBuscado] = useState(''); // [value, setValue]
+  const [usuarios, setUsuarios] = useState([]);
+  const [usuariosFiltrados, setUsuariosFiltrados] = useState(usuarios); 
+
+  // Função que fará com que rode apenas na primeira interação OU quando o valor do array mudar
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((resposta) => resposta.json())
+      .then((users) => setUsuarios(users))
+  }, []);
+
+  useEffect(() => {
+    const NovosUsuariosFiltrados = usuarios.filter((usuario) => {
+      return usuario.name.toLocaleLowerCase().includes(nomeBuscado);
+    })
+
+    setUsuariosFiltrados(NovosUsuariosFiltrados);
+  }, [usuarios, nomeBuscado])
+
+  const onNomeBuscadoChange = (event) => {
+    const nomeBuscadoString = event.target.value.toLocaleLowerCase();
+    setNomeBuscado(nomeBuscadoString);
+  }
+
   return (
     <div className="App">
       <h1 className='app-title'>Usuários Rolodex</h1>
-      {/*
-      <SearchBox ClassName='search-box' onChangeHandler={onNomeBuscadoChange} placeholder='Search Monster' />
-      <CardList usuarios={nomesDeUsuariosFiltrados} />
-      */}
+      <SearchBox
+        ClassName='search-box'
+        onChangeHandler={onNomeBuscadoChange}
+        placeholder='Search Monster' />
+      <CardList usuarios={usuariosFiltrados} />
     </div>
   )
 }
-
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       usuarios: [],
-//       nomeBuscado: ''
-//     }
-//   }
-
-//   onNomeBuscadoChange = (event) => {
-
-//     const nomeBuscado = event.target.value.toLocaleLowerCase();
-
-//     this.setState(() => {
-//       return { nomeBuscado };
-//     })
-
-//   }
-
-//   render() {
-
-//     //Inicializando os acessos ao state
-//     const { usuarios, nomeBuscado } = this.state;
-//     const { onNomeBuscadoChange } = this;
-
-//     //Constante que guarda o nome dos usuários filtrados
-//     const nomesDeUsuariosFiltrados = usuarios.filter((usuario) => {
-//       return usuario.name.toLocaleLowerCase().includes(nomeBuscado);
-//     })
-
-//     return (
-//       <div className="App">
-//         <h1 className='app-title'>Usuários Rolodex</h1>
-//         <SearchBox ClassName='search-box' onChangeHandler={ onNomeBuscadoChange } placeholder='Search Monster' />
-//         <CardList usuarios={ nomesDeUsuariosFiltrados } />
-//       </div>
-//     );
-//   }
-
-//   componentDidMount() {
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//       .then((resposta) => resposta.json())
-//       .then((users) =>
-//         this.setState(
-//           () => {
-//             return { usuarios: users };
-//           },
-//           () => {
-//             console.log(this.state);
-//           }
-//         )
-//       )
-//   }
-// }
 
 export default App;
